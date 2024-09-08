f9180c8e5f059d55bca81813fd0fb0f80c3dffda
maaya-sato <165452861+maaya-sato@users.noreply.github.com>
2024-09-08 17:54:52 +0900

    再マイグレーション

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // DB::table('employees')->insert([
+        //     ['name' => '太郎', 'email' => 'taro@techic.co.jp', 'hire_date' => '2024-01-01' ],
+        //     ['name' => '次郎', 'email' => 'jiro@techic.co.jp', 'hire_date' => '2024-01-01'],
+        //     ['name' => '三郎', 'email' => 'saburo@techic.co.jp', 'hire_date' => '2024-01-01'],
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
         // ]);
+        DB::table('skill_ranks')->insert([
+            ['name' => '上級SE'],
+            ['name' => '中級SE'],
+            ['name' => '初級SE'],
+           
+            
+        ]);
+        DB::table('departments')->insert([
+            ['name' => '開発部'],
+            ['name' => '営業部'],
+            ['name' => '総務部'],
+           
+            
+        ]);
+
     }
 }
