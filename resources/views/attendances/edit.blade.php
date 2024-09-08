abe447e48e274cf9c5c9a3aa8df634889d866f85
maaya-sato <165452861+maaya-sato@users.noreply.github.com>
2024-09-08 19:01:38 +0900

    Attendanceコントローラー修正

--- resources/views/attendances/edit.blade.php
+++ resources/views/attendances/edit.blade.php
@@ -24,13 +24,13 @@
             <!-- 出勤時刻 -->
             <div class="mb-3">
                 <label for="clock_in" class="form-label">出勤時刻</label>
-                <input type="datetime-local" name="clock_in" id="clock_in" class="form-control" value="{{ \Carbon\Carbon::parse($attendance->clock_in)->format('Y-m-d\TH:i') }}" required>
+                <input type="datetime-local" name="clock_in" id="clock_in" class="form-control" value="{{ \Carbon\Carbon::parse($attendance->clock_in)->format('Y-m-d H:i') }}" required>
             </div>
 
             <!-- 退勤時刻 -->
             <div class="mb-3">
                 <label for="clock_out" class="form-label">退勤時刻</label>
-                <input type="datetime-local" name="clock_out" id="clock_out" class="form-control" value="{{ \Carbon\Carbon::parse($attendance->clock_out)->format('Y-m-d\TH:i') }}" required>
+                <input type="datetime-local" name="clock_out" id="clock_out" class="form-control" value="{{ \Carbon\Carbon::parse($attendance->clock_out)->format('Y-m-d H:i') }}" required>
             </div>
 
             <button type="submit" class="btn btn-primary">更新</button>
